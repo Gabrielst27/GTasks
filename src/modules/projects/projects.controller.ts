ddf94d7afaaf8b941d030130125981cc654f876e
Gabriel Torres <gabrielst328@gmail.com>
2026-02-13 19:28:30 -0300

    feat(projects): implement controller methods

--- src/modules/projects/projects.controller.ts
+++ src/modules/projects/projects.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { ProjectRequestDto } from 'src/modules/projects/projects.dto';
+import { ProjectsService } from 'src/modules/projects/projects.service';
 
 @Controller('projects')
-export class ProjectsController {}
+export class ProjectsController {
+  constructor(private projectsService: ProjectsService) {}
+
+  @Get()
+  findAll() {
+    return this.projectsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.projectsService.findById(id);
+  }
+
+  @Post()
+  create(@Body() data: ProjectRequestDto) {
+    return this.projectsService.create(data);
+  }
+
+  @Put()
+  update(@Param('id') id: string, @Body() data: ProjectRequestDto) {
+    return this.projectsService.update(id, data);
+  }
+
+  @Delete()
+  delete(@Param('id') id: string) {
+    return this.projectsService.delete(id);
+  }
+}
--- src/modules/projects/projects.service.ts
+++ src/modules/projects/projects.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ProjectRequestDto } from 'src/modules/projects/projects.dto';
 
 @Injectable()
-export class ProjectsService {}
+export class ProjectsService {
+  findAll() {}
+  findById(id: string) {}
+  create(data: ProjectRequestDto) {}
+  update(id: string, data: ProjectRequestDto) {}
+  delete(id: string) {}
+}
