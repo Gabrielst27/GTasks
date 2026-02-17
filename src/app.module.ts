1e38e336149e3bebf83aaa6cacd62f996216fc4c
Gabriel Torres <gabrielst328@gmail.com>
2026-02-17 18:25:16 -0300

    infra/feat(app/shared): add config module, create shared and prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProjectsModule } from './modules/projects/projects.module';
+import { SharedModule } from './modules/shared/shared.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProjectsModule],
+  imports: [ProjectsModule, SharedModule, ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/projects/repositories/prisma/projects.repository.ts
+++
@@ -1,4 +0,0 @@
-import { IRepository } from 'src/common/repositories/repository.interface';
-import { ProjectEntity } from 'src/modules/projects/entities/project.entity';
-
-export interface IProjectRepository extends IRepository.Repository<ProjectEntity> {}
