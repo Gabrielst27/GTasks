ee884b0781c30760c526ea03a21677a0dcd98380
Gabriel Torres <gabrielst328@gmail.com>
2026-02-17 15:18:22 -0300

    feat(projects): create project entity

---
+++ src/modules/projects/entities/project.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, EntityProps } from 'src/common/entities/entity';
+
+export type ProjectEntityProps = {
+  name: string;
+  description: string;
+} & EntityProps;
+
+export class ProjectEntity extends Entity<ProjectEntityProps> {
+  constructor(
+    private readonly props: ProjectEntityProps,
+    id?: string,
+  ) {
+    //TODO: Create project entity validation
+    super(props, id);
+  }
+}
